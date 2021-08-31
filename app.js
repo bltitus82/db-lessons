2e8de8ead1c7c755105b1e1f27c4d2e4003dd87a
Brad Titus <bltitus@me.com>
2021-08-31 19:39:19 -0400

    notes on one-to-many and many-to-many

--- app.js
+++ app.js
@@ -31,20 +31,44 @@ const Profile = sequelize.define("Profile", {
     }
 })
 
-User.hasOne(Profile);
+User.hasOne(Profile, {
+    onDelete: "CASCADE"
+});
 Profile.belongsTo(User);
 /* 
 One to Many Relationships
-
+- one entry relates to multiple fields in another table
+- for example, a user has multiple orders
+- will use "hasMany" and "belongsTo"
 */
 
+const Order = sequelize.define("Order", {
+    shipDate: {
+        type: DataTypes.DATE
+    }
+})
+
+User.hasMany(Order);
+Order.belongsTo(User);
 /*
-
 Many To Many Relationships
-
+- multiple fields in one table can relate to multiple fields in another table
+- for example, Twitter - I follow many and have many follow me
+- will use "belongsToMany" both ways, and use "{through: "Table Name"}"
 */
+
+const Class = sequelize.define("Class", {
+    className: {
+        type: DataTypes.STRING
+    },
+    startDate: {
+        type: DataTypes.DATE
+    }
+})
+
+User.belongsToMany(Class, { through: "Users_Classes"});
+Class.belongsToMany(User, { through: "Users_Classes"});
 
 ;(async() => {
     await sequelize.sync({force: true});
-
 })();
